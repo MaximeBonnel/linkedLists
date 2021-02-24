c7f3d70c5570af746a25a939099b41701fb09bd6
MaximeBonnel <maxime.bonnel@student.yncrea.fr>
2021-02-24 14:24:27 +0100

    #

--- LinkedList.h
+++ LinkedList.h
@@ -12,14 +12,44 @@ typedef struct linkedList {
 
 // ---- Functions ----
 
+// **************************************************
+//  # Author: Bonnel Maxime
+//  # Modified time: 2021-02-24 14:05:46
+//  # Description: Initialisation of a new linked list
+// **************************************************
 struct linkedList * newLinkedList();
 
+// **************************************************
+//  # Author: Bonnel Maxime
+//  # Modified time: 2021-02-24 14:05:46
+//  # Description: Creation of a new link
+// **************************************************
 struct link * newLink(int data);
 
+// **************************************************
+//  # Author: Bonnel Maxime
+//  # Modified time: 2021-02-24 14:05:46
+//  # Description: Simple function to display a linked list
+// **************************************************
 int displayLinkedList(struct linkedList * LinkedList);
 
+// **************************************************
+//  # Author: Bonnel Maxime
+//  # Modified time: 2021-02-24 14:05:46
+//  # Description: Function to add a link at the end of the linked list
+// **************************************************
 int addLink(struct linkedList * LinkedList, struct link * Link);
 
+// **************************************************
+//  # Author: Bonnel Maxime
+//  # Modified time: 2021-02-24 14:05:46
+//  # Description: Function to remove a link in a linked list
+// **************************************************
 int rmlink(struct linkedList * LinkedList, struct link * Link);
 
+// **************************************************
+//  # Author: Bonnel Maxime
+//  # Modified time: 2021-02-24 14:05:46
+//  # Description: Function to switch two following link in the linked list
+// **************************************************
 int switchLink(struct linkedList * LinkedList, struct link * fLink, struct link * sLink);
--- README.md
+++ README.md
@@ -1,4 +1,23 @@
 # linkedLists
 
-> salut
-> salut
+> Code with basic functions for linked lists
+
+## Functions :
+
+### newLinkedList
+> Initialisation of a new linked list
+
+### newLink
+> Creation of a new link
+
+### displayLinkedList
+> Simple function to display a linked list
+
+### addLink
+> Function to add a link at the end of the linked list
+
+### rmLink
+> Function to remove a link in a linked list
+
+### switchLink
+> Function to switch two following link in the linked list
